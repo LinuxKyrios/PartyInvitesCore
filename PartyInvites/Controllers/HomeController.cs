47ca2e389663124e94cc33d2cf46156291006899
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-07 22:17:52 +0200

    Created first testing view in MyView.cshtml

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello world of Masters of the Universe";
+            return View("MyView");
         }
     }
 }
