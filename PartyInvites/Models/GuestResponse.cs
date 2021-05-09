94a1ba34ebf5de42681ef074b34fa1926ebd4038
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-09 21:33:46 +0200

    Created form in RsvpForm.cshtml file

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -3,6 +3,7 @@ namespace PartyInvites.Models
     public class GuestResponse
     {
         public string Name { get; set; }
+        public string Nickname { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public bool? WillAttend { get; set; } //Attribute WillAttend is marked as nullable in boolean type.
